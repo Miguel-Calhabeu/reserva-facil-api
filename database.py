59504d2f56a42e46e8b3d6dc30970d2dc3ea0ac9
Miguel-Calhabeu <miguel.calhabeu@gmail.com>
2025-12-01 00:17:01 -0300

    implement ipv4 db connection

--- database.py
+++ database.py
@@ -23,6 +23,7 @@ def get_db_connection():
         host=os.environ.get("DB_HOST"),
         database=os.environ.get("DB_NAME"),
         user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD")
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT", 5432)
     )
     return conn
